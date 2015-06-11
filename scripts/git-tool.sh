d700440e80de6d8def76f45d0b18554e12ce3462
JakeYang <add358@gmail.com>
2015-06-11 14:58:12 +0800

    添加wndr3800默认配置文件，添加git工具

---
+++ scripts/git-tool.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#=======================================
+# File  : git-tool.sh
+# Brief : 统一操作多个git仓库
+# Author: JakeYang <add358@gmail.com>
+# Log   : 2015-03-04 create
+#=======================================
+
+# checkout  {{{1
+checkout() {
+	git checkout $1
+}
+
+# pull  {{{1
+pull() {
+	git checkout master
+	git pull
+}
+
+# push  {{{1
+push() {
+	git checkout master
+	git push oschina
+}
+
+# usage {{{1
+usage()
+{
+	echo "Usage: `basename $0` [chk remote|pull|push] "
+	exit
+}
+
+# main {{{1
+remote=$1
+operation=$2
+OPENWRT_DIR=$(dirname `readlink -f $0`)/..
+LUCI_DIR=${OPENWRT_DIR}/feeds/luci
+MANAGEMENT_DIR=${OPENWRT_DIR}/feeds/management
+PACKAGE_DIR=${OPENWRT_DIR}/feeds/packages
+ROUTING_DIR=${OPENWRT_DIR}/feeds/routing
+TELEPHONY_DIR=${OPENWRT_DIR}/feeds/telephony
+
+git_dirs="${OPENWRT_DIR} ${LUCI_DIR} ${MANAGEMENT_DIR} ${PACKAGE_DIR} ${ROUTING_DIR} ${TELEPHONY_DIR}"
+
+for dir in ${git_dirs}; do
+	cd ${dir}
+	git $*
+done
